01ee02c9ca7fe44aa3bb3f4969f73810aa1371b8
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2023-12-26 17:55:01 -0500

    Added Header and description to Home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>
+            Discover & Share
+            <br className='max-md:hidden'/>
+            <span className='colorful_gradient text-center'> AI-Powered Prompts</span>
+        </h1>
+
+        <p className='desc text-center'>
+            Welcome to Prompt Synergy, your go-to app for sparking creativity and inspiration!
+        </p>
+    </section>
   )
 }
 
